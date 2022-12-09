1ee7ea4109cd554020545c661fbfe4bd1e17d49c
ray <ray@ray.com>
2022-12-09 13:14:07 +0000

    controller

--- app/Http/Controllers/AccessController.php
+++ app/Http/Controllers/AccessController.php
@@ -3,63 +3,61 @@
 namespace App\Http\Controllers;
 
 use App\Models\Access;
-use App\Models\TypeAccess;
-use App\Models\User;
 use Illuminate\Http\Request;
 
 class AccessController extends Controller
 {
-    public function index($user_id){
-        $accesses = Access::where('user_id', $user_id)->get();
+    public function index($client_id){
+        $accesses = Access::where('client_id', $client_id)->get();
         $result = [];
         foreach($accesses as $access){
             array_push($result, $access->only('id', 'name'));
         }
         return response()->json($result);
     }
-    public function show($user_id, $id) {
-        $access = Access::where('user_id', $user_id)->find($id);
+    public function show($client_id, $id) {
+        $access = Access::where('client_id', $client_id)->find($id);
 
-        $result = $access->only('id', 'name', 'type_id', 'user_id', 'data');
+        $result = $access->only('id', 'client_id', 'sample_id', 'name', 'data');
         $result['data'] = json_decode($result['data']);
 
         return response()->json($result);
     }
 
-    public function store(Request $request, $user_id) {
+    public function store(Request $request, $client_id) {
         $input = $request->all();
 
         $access = Access::create([
             'name' => $input['name'],
-            'type_id' => $input["type_id"],
-            'user_id' => $user_id,
-            'data' => TypeAccess::find($input['type_id'])->struct
+            'sample_id' => $input["sample_id"],
+            'client_id' => $client_id,
+            'data' => json_encode($input['data'])
         ]);
 
-        $result = $access->only('id', 'name', 'type_id', 'user_id', 'data');
+        $result = $access->only('id', 'client_id', 'sample_id', 'name', 'data');
         $result['data'] = json_decode($result['data']);
 
         return response()->json($result);
     }
 
-    public function update(Request $request, $user_id, $id){
+    public function update(Request $request, $client_id, $id){
         $input = $request->all();
-        $access = Access::where('user_id', $user_id)->find($id);
+        $access = Access::where('client_id', $client_id)->find($id);
 
-        $access->type_id = $input['type_id'];
+        $access->sample_id = $input['sample_id'];
         $access->name = $input['name'];
         $access->data = json_encode($input['data']);
         $access->save();
 
-        $result = $access->only('id', 'name', 'type_id', 'user_id', 'data');
+        $result = $access->only('id', 'client_id', 'sample_id', 'name', 'data');
         $result['data'] = json_decode($result['data']);
 
         return response()->json($result);
     }
-    public function destroy($user_id, $id){
-        $access = Access::where('user_id', $user_id)->find($id);
+    public function destroy($client_id, $id){
+        $access = Access::where('client_id', $client_id)->find($id);
 
-        $result = $access->only('id', 'name', 'type_id', 'user_id', 'data');
+        $result = $access->only('id', 'client_id', 'sample_id', 'name', 'data');
         $result['data'] = json_decode($result['data']);
 
         $access->delete();
--- app/Http/Controllers/AccessSampleController.php
+++ app/Http/Controllers/AccessSampleController.php
@@ -2,9 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AccessSample;
 use Illuminate\Http\Request;
 
 class AccessSampleController extends Controller
 {
-    //
+    public function index(){
+        $samples = AccessSample::all();
+        $result = [];
+        foreach($samples as $sample){
+            array_push($result, $sample->only('id', 'name'));
+        }
+        return response()->json($result);
+    }
+    public function show($id) {
+        $sample = AccessSample::find($id);
+
+        $result = $sample->only('id', 'type_id', 'name', 'data');
+        $result['data'] = json_decode($result['data']);
+
+        return response()->json($result);
+    }
+
+    public function store(Request $request) {
+        $input = $request->all();
+        $sample = AccessSample::create([
+            'name' => $input['name'],
+            'data' => json_encode($input['data'])
+        ]);
+
+        $result = $sample->only('id', 'type_id', 'name', 'data');
+        $result['data'] = json_decode($result['data']);
+
+        return response()->json($result);
+    }
+
+    public function update(Request $request, $id){
+        $input = $request->all();
+        $sample = AccessSample::find($id);
+
+        $sample->name = $input['name'];
+        $sample->data = json_encode($input['data']);
+        $sample->save();
+
+        $result = $sample->only('id', 'type_id', 'name', 'data');
+        $result['data'] = json_decode($result['data']);
+
+        return response()->json($result);
+    }
+    public function destroy($id){
+        $sample = AccessSample::find($id);
+
+        $result = $sample->only('id', 'type_id', 'name', 'data');
+        $result['data'] = json_decode($result['data']);
+        
+        $sample->delete();
+        return response()->json($result);
+    }
 }
